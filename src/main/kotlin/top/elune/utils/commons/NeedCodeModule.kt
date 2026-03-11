0283d92306a0fc5cec2b77cff4879eb8c6d65571
OCTSJimmy <octsjimmy@gmail.com>
2026-03-11 14:57:22 +0800

    feat(commons): 添加NeedCodeModule和NeedListManager类
    实现NeedCodeModule类用于管理站点代码和模块代码的组合，以及NeedListManager类用于从CSV文件加载并管理NeedCodeModule实例。这些类将用于处理站点和模块代码的关联关系。

---
+++ src/main/kotlin/top/elune/utils/commons/NeedCodeModule.kt
@@ -0,0 +1,23 @@
+package cn.org.ncrcnd.bigdata.ct_genai.dicom.selector.commons
+
+@Suppress("unused")
+class NeedCodeModule @JvmOverloads constructor(
+    private var mSiteCode: String,
+    private var mCode: String,
+    var codeModule: CodeModule? = CodeManager.INSTANCE["$mSiteCode-$mCode"],
+) {
+    var siteCode: String
+        get() = mSiteCode
+        set(siteCode) {
+            mSiteCode = siteCode
+            codeModule = CodeManager.INSTANCE["$siteCode-$mCode"]
+        }
+    var code: String
+        get() = mCode
+        set(code) {
+            mCode = code
+            codeModule = CodeManager.INSTANCE["$mSiteCode-$code"]
+        }
+    val fullCode: String
+        get() = String.format("%s-%s", mSiteCode, mCode)
+}
